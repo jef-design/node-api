239fbc5d6fcc99d9a23775956b13681505dcafb9
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-09 17:02:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ app.get('/', (req, res) => {
     res.send('ok')
 })
 
-mongoose.connect('mongodb+srv://jef1234:jef1234@mern-blogdb.cwxjvqt.mongodb.net/?retryWrites=true&w=majority')
-.then(() => {
-    app.listen(5000, (req, res) => {
-        console.log("connected to server")
-    })
-})
+app.listen(5000, (req, res) => {
+console.log("connected to server")
+ })
 
